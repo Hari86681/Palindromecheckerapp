5b64399cd993bbb5902852ffa9905ed1e5357ebe
Hari86681 <muhariharan333@gmail>
2026-03-06 16:15:43 +0530

    UC5:stackreverse

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
-class PalindromeChecker {
+import java.util.Stack;
+class Palindrome {
         public static void main(String[] args) {
             String str = "madam";
-            char[] arr = str.toCharArray();
-            boolean isPalindrome = true;
-            for (int i = 0; i < arr.length / 2; i++) {
-                if (arr[i] != arr[arr.length - 1 - i]) {
-                    isPalindrome = false;
-                    break;
-                }
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < str.length(); i++) {
+                stack.push(str.charAt(i));
             }
-            if (isPalindrome) {
+            String reversed = "";
+            while (!stack.isEmpty()) {
+                reversed = reversed + stack.pop();
+            }
+            if (str.equals(reversed)) {
                 System.out.println("The string is a palindrome");
             } else {
                 System.out.println("The string is not a palindrome");
