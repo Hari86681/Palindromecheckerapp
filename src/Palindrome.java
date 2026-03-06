d95d975afe11aea33e0abec72afdffc7808fc83f
Hari86681 <muhariharan333@gmail>
2026-03-06 16:35:54 +0530

    UC8:Singly linked list

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,19 +1,54 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
-        class Palindrome {
-            public static void main(String[] args) {
-                Deque<Integer> deque = new ArrayDeque<>();
-                deque.addFirst(10);
-                deque.addLast(20);
-                deque.addLast(30);
-                int front = deque.peekFirst();
-                int rear = deque.peekLast();
-                if (front == rear) {
-                    System.out.println("Front and Rear elements are equal");
-                } else {
-                    System.out.println("Front element: " + front);
-                    System.out.println("Rear element: " + rear);
-                    System.out.println("Front and Rear elements are different");
-                }
-            }
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+class PalindromeLinkedList {
+    Node head;
+
+    void add(char data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
         }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    boolean isPalindrome() {
+        String original = "";
+        String reverse = "";
+        Node temp = head;
+
+        while (temp != null) {
+            original += temp.data;
+            reverse = temp.data + reverse;
+            temp = temp.next;
+        }
+
+        return original.equals(reverse);
+    }
+
+    public static void main(String[] args) {
+        PalindromeLinkedList list = new PalindromeLinkedList();
+
+        String str = "madam";
+        for (int i = 0; i < str.length(); i++) {
+            list.add(str.charAt(i));
+        }
+
+        if (list.isPalindrome()) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
