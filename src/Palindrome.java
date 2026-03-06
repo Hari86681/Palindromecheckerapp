68d396e6fb30b2e8095eb211295f7e63cbbb41e5
Hari86681 <muhariharan333@gmail>
2026-03-06 16:44:33 +0530

    UC10:ignoreusecase

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,16 +1,15 @@
 class Palindrome {
-    static boolean isPalindrome(String str, int start, int end) {
-        if (start >= end)
-            return true;
-        if (str.charAt(start) != str.charAt(end))
-            return false;
-        return isPalindrome(str, start + 1, end - 1);
-    }
     public static void main(String[] args) {
-        String str = "madam";
-        if (isPalindrome(str, 0, str.length() - 1))
+        String str = "Never Odd Or Even";
+        str = str.replaceAll("\\s", "").toLowerCase();
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed = reversed + str.charAt(i);
+        }
+        if (str.equals(reversed)) {
             System.out.println("Palindrome");
-        else
+        } else {
             System.out.println("Not a Palindrome");
+        }
     }
 }
